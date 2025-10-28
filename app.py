7906383567a86511cb71c478383de276f3cfb1c5
Baylie Schnieder <bschnieder@uiowa.edu>
2025-10-28 15:13:50 -0500

    what we did in class

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 # module 10 - Flask Application
 # Baylie Schnieder 10/28/2025
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!!"
+def home():
+    return render_template('index.html') 
 
 @app.route("/about")
 def about():
-    return "This is Baylie's about page"
+    return render_template('about.html')
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", port=5002, debug=True)
